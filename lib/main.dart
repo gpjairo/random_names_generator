ea43000f2fe502afffabcca164ab5d0a0da7ff54
gpjairo <gpjairo@gmail.com>
2018-12-31 01:37:05 +0000

    Update main.dart
    Added new screen to display all favourites + major refactor to reuse widgets

--- lib/main.dart
+++ lib/main.dart
@@ -12,43 +12,37 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Startup names generator'),
-        ),
-        body: new Center(
-          child: new RandomWords()
-        )
-      )
+      home: new WordPairListPage()
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
+class WordPairListPage extends StatefulWidget {
+  @override
+  WordPairListPageState createState() => new WordPairListPageState();
+}
+
+class WordPairListPageState extends State<WordPairListPage> {
   final List<WordPair> _wordPairList = <WordPair>[];
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
-  final Set<WordPair> _favouritedWordPairs = new Set<WordPair>();
 
   Widget _buildRow(WordPair wordPair) {
-    final isFavourited = FavouriteWordPairsRepository.getRepository().contains(wordPair);
-    final icon = isFavourited ? Icons.favorite : Icons.favorite_border;
-    final color = isFavourited ? Colors.red : null;
     final GestureTapCallback onTapCallback = () {
       setState(() { // To refresh the UI
         FavouriteWordPairsRepository.getRepository().toggleFavourite(wordPair);
       });
     };
-
-    return new ListTile(
-      title: new Text(
-        wordPair.asPascalCase,
-        style: _biggerFont),
-      trailing: new Icon(icon, color:color),
-      onTap: onTapCallback,
-    );
+    return new WordPairListRow(wordPair, onTapCallback);
   }
 
-  Widget _buildSuggestions() {
+  @override
+  Widget build(BuildContext context) { 
+    final _navigateToFavouritesScreen = () {
+      Navigator.of(context).push(
+        new MaterialPageRoute<void>(builder: (context) => FavouritesListPage()),
+      );
+    };
+      
     IndexedWidgetBuilder itemBuilder = (BuildContext context, int index) {
       if (index.isOdd) {
         return new Divider();
@@ -57,25 +51,85 @@ class RandomWordsState extends State<RandomWords> {
       if (i >= _wordPairList.length) {
         _wordPairList.addAll(generateWordPairs().take(10));
       }
-
       return _buildRow(_wordPairList[i]);
     };
 
-    return new ListView.builder(
-      padding: const EdgeInsets.all(8.0),
-      itemBuilder: itemBuilder,
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('Startup names generator'),
+        actions: <Widget>[
+          new IconButton(icon: const Icon(Icons.list), onPressed: _navigateToFavouritesScreen),
+        ],
+      ),
+      body: new Center(
+        child: new ListView.builder(
+          padding: const EdgeInsets.all(8.0),
+          itemBuilder: itemBuilder,
+        )
+      )
     );
-  }
-
-  @override
-  Widget build(BuildContext context) {  
-    return _buildSuggestions();
   }
 }
 
-class RandomWords extends StatefulWidget {
+class FavouritesListPage extends StatefulWidget {
   @override
-  RandomWordsState createState() => new RandomWordsState();
+  FavouritesListPageState createState() => new FavouritesListPageState();
+}
+class FavouritesListPageState extends State<FavouritesListPage> {
+  final List<WordPair> _wordPairList = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+    Widget _buildRow(WordPair wordPair) {
+      final GestureTapCallback onTapCallback = () {
+        setState(() { // To refresh the UI
+          FavouriteWordPairsRepository.getRepository().toggleFavourite(wordPair);
+        });
+      };
+      return new WordPairListRow(wordPair, onTapCallback);
+    }
+
+  @override
+  Widget build(BuildContext context) {  
+    List<WordPair> favouritesList = FavouriteWordPairsRepository.getRepository().getList();
+    IndexedWidgetBuilder itemBuilder = (BuildContext context, int index) {
+      return _buildRow(favouritesList[index]);
+    };
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('Favourites'),
+      ),
+      body: new Center(
+        child: new ListView.builder(
+          padding: const EdgeInsets.all(8.0),
+          itemCount: favouritesList.length,
+          itemBuilder: itemBuilder,
+        )
+      )
+    );
+  }
+}
+
+class WordPairListRow extends StatelessWidget {
+  WordPairListRow(this._wordPair, this._actionCallback);
+  final WordPair _wordPair;
+  final GestureTapCallback _actionCallback;
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    final isFavourited = FavouriteWordPairsRepository.getRepository().contains(_wordPair);
+    final icon = isFavourited ? Icons.favorite : Icons.favorite_border;
+    final color = isFavourited ? Colors.red : null;
+
+    return new ListTile(
+      title: new Text(
+        _wordPair.asPascalCase,
+        style: _biggerFont),
+      trailing: new Icon(icon, color:color),
+      onTap: _actionCallback,
+    );
+  }
 }
 
 
